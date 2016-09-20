a420cb909ba6b704a08cb3ded8a4b30a00f0f4f5
jiayuliu27 <jiayuliu@umich.edu>
2016-09-19 21:37:31 -0400

    update files

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-app.listen(3000, function(err) {
-	if(err) throw err;
-	console.log("server listening");
+// other dependencies
+var chalk = require("chalk");
+var morgan = require("morgan");
+
+
+var errorStyle = chalk.bold.red;
+var requestStyle = chalk.blue.underline;
+var responseStyle = chalk.yellow;
+var messageStyle = chalk.gray;
+
+app.use(morgan("combined"));
+// these requests are processed in order
+// will not execute the next matched one unless next()
+app.get('/', function(req, res, next) {
+	chalk.blue.bgRed.bold("Hello world!");
+	console.log(chalk.green("Hello '/' location"), responseStyle(res.statusCode.toString()));
+	res.send("Welcome!");
+	next();
 });
 
-app.get('/', function(req, res, next) {
-	res.send("Welcome!");
+app.use("/special/", function(req, res, next) {
+	console.log(messageStyle("you've reached a special area"));
+	next();
+});
+
+app.use(function(req, res, next) {
+	// accepts all requests from any url
+	console.log(messageStyle("Accepts all requests"), requestStyle(req.method.toString()), requestStyle(req.path.toString()), responseStyle(res.statusCode.toString()));
+	// res.send("Accepts all requests");
+	next();
+});
+
+app.listen(3000, function(err) {
+	if(err) {
+		console.log(errorStyle(err.toString()));
+		throw err;
+	}
+	console.log(messageStyle("server listening"));
 });
